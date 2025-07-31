8d8a4138a7c4b67178ee9cfe275a7352195959d2
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-07-31 15:41:23 +0300

    feat: add remote data source and retrofit service for movie api

---
+++ core/network/src/main/java/com/emirerkut/network/source/MovieRemoteDataSource.kt
@@ -0,0 +1,9 @@
+package com.emirerkut.network.source
+
+import com.emirerkut.network.model.MovieResponseDTO
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+
+interface MovieRemoteDataSource {
+    fun getPopularMovies(language: String): Flow<Response<MovieResponseDTO>>
+}
