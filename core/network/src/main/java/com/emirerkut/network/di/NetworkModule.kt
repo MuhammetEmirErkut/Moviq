a999425b00181ae9f121e8af1361d42ef4343830
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-07-31 15:41:38 +0300

    feat: add dependency injection modules for network layer

---
+++ core/network/src/main/java/com/emirerkut/network/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.emirerkut.network.di
+
+import com.emirerkut.network.interceptor.ApiKeyInterceptor
+import com.emirerkut.network.util.Constants
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+    ) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(ApiKeyInterceptor())
+            .build()
+    }
+}
