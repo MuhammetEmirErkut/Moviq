f30e0d43be6c693c6c91e96c6a2089cbeaa07bdd
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-07-31 15:42:04 +0300

    feat: add flow extension for rest api error handling

---
+++ core/network/src/main/java/com/emirerkut/network/util/FlowExt.kt
@@ -0,0 +1,33 @@
+package com.emirerkut.network.util
+
+import com.emirerkut.common.model.ErrorType
+import com.emirerkut.common.model.Failure
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.map
+import retrofit2.Response
+
+fun <T, Model> Flow<Response<T>>.asRestApiCall(mapper: (T) -> Model): Flow<Model> =
+    map { respone ->
+        if (respone.isSuccessful.not()) {
+            when(respone.code()) {
+                400 -> throw Failure(ErrorType.BAD_REQUEST)
+                401 -> throw Failure(ErrorType.UNAUTHORIZED)
+                403 -> throw Failure(ErrorType.FORBIDDEN)
+                404 -> throw Failure(ErrorType.NOT_FOUND)
+                429 -> throw Failure(ErrorType.TOO_MANY_REQUESTS)
+                in 500..599 -> throw Failure(ErrorType.SERVER_ERROR)
+                else -> throw Failure(ErrorType.UNKNOWN)
+            }
+        }
+
+        val body = respone.body() ?: throw Failure(ErrorType.EMPTY_RESPONSE)
+
+        try {
+            mapper(body)
+        } catch (e: Exception) {
+            cancellable()
+            throw Failure(ErrorType.SERIALIZATION)
+        }
+
+    }
