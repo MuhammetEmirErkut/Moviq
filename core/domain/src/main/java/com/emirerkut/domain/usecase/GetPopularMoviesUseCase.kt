a53114e2cfda81be92b29fa4f855cbf0c522eedb
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-08-01 17:30:35 +0300

    feat: implement getting popular movies use case

---
+++ core/domain/src/main/java/com/emirerkut/domain/usecase/GetPopularMoviesUseCase.kt
@@ -0,0 +1,12 @@
+package com.emirerkut.domain.usecase
+
+import com.emirerkut.model.Movie
+import com.emirerkut.data.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetPopularMoviesUseCase @Inject constructor(
+    private val repository: MovieRepository
+) {
+    fun execute(language: String): Flow<Movie> = repository.getPopularMovies(language)
+}
