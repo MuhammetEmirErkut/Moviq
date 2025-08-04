afdae6701a33fed4b2e02fed2fe024d420961f7c
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-08-04 17:23:35 +0300

    fix: corrected the request url

--- core/network/src/main/java/com/emirerkut/network/util/Constants.kt
+++ core/network/src/main/java/com/emirerkut/network/util/Constants.kt
@@ -2,5 +2,5 @@ package com.emirerkut.network.util
 
 object Constants {
     const val LANGUAGE = "en"
-    const val BASE_URL = "https://api.themoviedb.org/"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
 }
