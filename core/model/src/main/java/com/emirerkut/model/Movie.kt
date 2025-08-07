77a675182e3d302d9cfeea0d6c46eefafc9c3f6d
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-08-07 15:09:30 +0300

    feat: add poster path

--- core/model/src/main/java/com/emirerkut/model/Movie.kt
+++ core/model/src/main/java/com/emirerkut/model/Movie.kt
@@ -3,5 +3,6 @@ package com.emirerkut.model
 data class Movie(
     val id: Int,
     val originalTitle: String?,
-    val originalLanguage: String?
+    val originalLanguage: String?,
+    val posterPath: String?
 )
