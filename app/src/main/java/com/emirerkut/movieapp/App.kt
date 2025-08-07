dfbf04c4cc9ffa2b9e686417c00936049023ce06
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-08-07 15:07:33 +0300

    feat: add snack bar

--- app/src/main/java/com/emirerkut/movieapp/App.kt
+++ app/src/main/java/com/emirerkut/movieapp/App.kt
@@ -3,20 +3,39 @@ package com.emirerkut.movieapp
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import com.emirerkut.movieapp.navigation.MovieAppNavHost
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun App(
     appState: AppState = rememberAppState()
 ) {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    val onShowSnackbar: suspend (String, String?) -> Unit = { message, actionLabel ->
+        coroutineScope.launch {
+            snackbarHostState.showSnackbar(
+                message = message,
+                actionLabel = actionLabel
+            )
+        }
+    }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-    ){
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ) {
         MovieAppNavHost(
             appState = appState,
+            onShowSnackbar = onShowSnackbar,
             modifier = Modifier.padding(it)
         )
     }
