81b8465a420290b5beaddf19ec558f9ef79a9d1d
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-08-07 15:13:39 +0300

    feat: add movie poster url

--- core/network/src/main/java/com/emirerkut/network/util/Constants.kt
+++ core/network/src/main/java/com/emirerkut/network/util/Constants.kt
@@ -3,4 +3,5 @@ package com.emirerkut.network.util
 object Constants {
     const val LANGUAGE = "en"
     const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 }
