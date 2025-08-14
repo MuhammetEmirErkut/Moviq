dfc5d8c6b3d87accd67be4341a590092f9a435fc
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-08-14 12:15:30 +0300

    feat: search screen added scaffold

--- app/src/main/java/com/emirerkut/movieapp/App.kt
+++ app/src/main/java/com/emirerkut/movieapp/App.kt
@@ -1,24 +1,33 @@
 package com.emirerkut.movieapp
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.emirerkut.movieapp.navigation.MovieAppNavHost
+import com.emirerkut.search.SearchScreen
+import com.emirerkut.search.SearchViewModel
 import kotlinx.coroutines.launch
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App(
     appState: AppState = rememberAppState()
 ) {
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+    val viewModel: SearchViewModel = hiltViewModel()
+    val searchState by viewModel.uiState.collectAsState()
 
     val onShowSnackbar: suspend (String, String?) -> Unit = { message, actionLabel ->
         coroutineScope.launch {
@@ -31,12 +40,20 @@ fun App(
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        snackbarHost = { SnackbarHost(snackbarHostState) }
-    ) {
-        MovieAppNavHost(
-            appState = appState,
-            onShowSnackbar = onShowSnackbar,
-            modifier = Modifier.padding(it)
-        )
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+    ) { paddingValues ->
+        Column{
+            SearchScreen(
+                searchState = searchState,
+                viewModel = viewModel,
+                onEvent = { event -> viewModel.onEvent(event) },
+            )
+
+            MovieAppNavHost(
+                appState = appState,
+                onShowSnackbar = onShowSnackbar,
+                modifier = Modifier.padding(paddingValues)
+            )
+        }
     }
 }
