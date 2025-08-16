4ede9dd1e3bc330fbdcdbf0033d0011fe2d8717e
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-08-16 11:29:35 +0300

    feat: movie section component integrated

--- feature/home/src/main/java/com/emirerkut/home/HomeScreen.kt
+++ feature/home/src/main/java/com/emirerkut/home/HomeScreen.kt
@@ -1,66 +1,40 @@
 package com.emirerkut.home
 
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.emirerkut.home.model.HomeState
-import com.emirerkut.ui.components.ErrorScreen
-import com.emirerkut.ui.components.LoadingScreen
-import com.emirerkut.ui.components.SectionWithMovieList
-
+import com.emirerkut.home.model.HomeUiState
+import com.emirerkut.home.model.toUiState
+import com.emirerkut.ui.components.MovieSection
 
 @Composable
 fun HomeScreen(
-    homeState: HomeState,
+    homeState: HomeUiState,
     viewModel: HomeViewModel = hiltViewModel(),
     onEvent: (HomeScreenEvent) -> Unit,
     whenErrorOccured: suspend (Throwable, String?) -> Unit
 ) {
-    Box(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.background)
+    Column(
+        modifier = Modifier.background(MaterialTheme.colorScheme.background)
     ) {
-        AnimatedContent(
-            targetState = homeState,
-            modifier = Modifier.align(Alignment.Center)
-        ) {
-            when (it) {
-                is HomeState.Loading -> {
-                    LoadingScreen()
-                }
+        MovieSection(
+            title = stringResource(R.string.popular_movies),
+            state = homeState.popular.toUiState(),
+            onRetry = { viewModel.retryLoadPopularMovies() },
+            whenErrorOccured = whenErrorOccured
+        )
 
-                is HomeState.Success -> {
-                    Column {
-                        SectionWithMovieList(
-                            title = stringResource(R.string.popular_movies),
-                            movies = it.movies,
-                            onRetry = { viewModel.retry() }
-                        )
-                    }
-                }
-
-
-                is HomeState.Error -> {
-                    ErrorScreen(
-                        whenErrorOccured = whenErrorOccured,
-                        failure = it.failure,
-                        onTryAgainClick = { onEvent(HomeScreenEvent.OnTryAgainClick) },
-                    )
-                }
-            }
-        }
-
+        MovieSection(
+            title = stringResource(R.string.top_rated_movies),
+            state = homeState.topRated.toUiState(),
+            onRetry = { viewModel.retryLoadTopRatedMovies() },
+            whenErrorOccured = whenErrorOccured
+        )
     }
 }
-
-
-
 
 
