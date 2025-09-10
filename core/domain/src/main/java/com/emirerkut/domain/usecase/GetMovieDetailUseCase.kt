0eafd01839ba1ef6ea4a34463c04d4a3a1a78186
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-09-10 12:45:32 +0300

    feat: add GetMovieDetailUseCase and wire in DI

--- core/domain/src/main/java/com/emirerkut/domain/usecase/GetMovieDetailUseCase.kt
+++ core/domain/src/main/java/com/emirerkut/domain/usecase/GetMovieDetailUseCase.kt
@@ -1,2 +1,14 @@
 package com.emirerkut.domain.usecase
 
+import com.emirerkut.data.repository.MovieRepository
+import com.emirerkut.model.Movie
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetMovieDetailUseCase @Inject constructor(
+    private val repository: MovieRepository
+) {
+    operator fun invoke(movieId: Int): Flow<Movie> {
+        return repository.getMovieDetail(movieId)
+    }
+}
