7264b1a96bdc07c48f77f6c70ad6444f864a87e4
Muhammet Emir Erkut <muhammetemir.erkut@gmail.com>
2025-09-14 16:09:12 +0300

    feat: add properties detail data flow

--- core/model/src/main/java/com/emirerkut/model/Movie.kt
+++ core/model/src/main/java/com/emirerkut/model/Movie.kt
@@ -5,5 +5,30 @@ data class Movie(
     val originalTitle: String?,
     val originalLanguage: String?,
     val posterPath: String?,
-    val overview: String?
+    val overview: String?,
+    val releaseDate: String?,
+    val runtime: Int?,
+    val genres: List<Genre>?,
+    val spokenLanguages: List<SpokenLanguage>?,
+    val voteAverage: Double?,
+    val voteCount: Int?,
+    val cast: List<Cast>?
+)
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
+
+data class SpokenLanguage(
+    val englishName: String?,
+    val iso6391: String?,
+    val name: String?
+)
+
+data class Cast(
+    val id: Int,
+    val name: String?,
+    val character: String?,
+    val profilePath: String?
 )
